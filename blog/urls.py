c7edfc417f83afb37c0d962160889052b7e5c2f9
Anugya <anugyakans@gmail.com>
2017-12-30 19:36:38 +0530

    HTML,CSS

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post_list/', views.post_list)]
+    path('post_list/', views.post_list, name = 'post_list'),
+    path(r'^post/(?P<pk>\d+)/$', views.post_detail,name = 'post_detail')
+    ]
